e186ffc449365b43a8122155721a9d1475e60d8d
Kamyar <kamyar.hasanzadeh@gmail.com>
2026-03-14 15:10:05 +0200

    Add initial Sphinx configuration in conf.py

---
+++ docs/conf.py
@@ -0,0 +1,11 @@
+project = "Activity space tools"
+author = "Kamyar Hasanzadeh"
+release = "0.1"
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+html_theme = "furo"
+html_static_path = ["_static"]
